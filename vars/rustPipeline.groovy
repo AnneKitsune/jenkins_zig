3148c53001d07879bbd69b560d87551362f4e985
AnneKitsune <annekitsunefox@gmail.com>
2025-06-12 17:18:22 -0400

    Revert "attempt to fix agent selection"
    This reverts commit 92e02f6733afb57988d9f00ad2dcef1a9abf5621.

--- vars/rustPipeline.groovy
+++ vars/rustPipeline.groovy
@@ -16,36 +16,34 @@ def call(Map config = [:]) {
     for (os in config.osList) {
         branches[os] = {
             node(os) {
-                agent "${os}" {
-                    stage("${os} Clone") {
-                        checkout scm
+                stage("${os} Clone") {
+                    checkout scm
+                }
+                stage("${os} Install Rust") {
+                    if (isUnix()) {
+                        sh "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain ${config.rustVersion}"
+                        env.PATH = "${env.HOME}/.cargo/bin:${env.PATH}"
+                    } else {
+                        bat "curl -sSf -o rustup-init.exe https://win.rustup.rs"
+                        bat "rustup-init.exe -y --default-toolchain ${config.rustVersion}"
+                        env.PATH = "${env.USERPROFILE}\\.cargo\\bin;${env.PATH}"
                     }
-                    stage("${os} Install Rust") {
-                        if (isUnix()) {
-                            sh "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain ${config.rustVersion}"
-                            env.PATH = "${env.HOME}/.cargo/bin:${env.PATH}"
-                        } else {
-                            bat "curl -sSf -o rustup-init.exe https://win.rustup.rs"
-                            bat "rustup-init.exe -y --default-toolchain ${config.rustVersion}"
-                            env.PATH = "${env.USERPROFILE}\\.cargo\\bin;${env.PATH}"
-                        }
+                }
+                stage("${os} Compile") {
+                    rustCompile(config)
+                }
+                stage("${os} Test") {
+                    rustTest(config)
+                }
+                if (config.enableBenchmarks) {
+                    stage("${os} Benchmark") {
+                        rustBenchmark(config)
                     }
-                    stage("${os} Compile") {
-                        rustCompile(config)
-                    }
-                    stage("${os} Test") {
-                        rustTest(config)
-                    }
-                    if (config.enableBenchmarks) {
-                        stage("${os} Benchmark") {
-                            rustBenchmark(config)
-                        }
-                    }
-                    stage("${os} Archive Artifacts") {
-                        if (config.artifactPatterns) {
-                            def pattern = config.artifactPatterns.join(',')
-                            archiveArtifacts artifacts: pattern, allowEmptyArchive: true
-                        }
+                }
+                stage("${os} Archive Artifacts") {
+                    if (config.artifactPatterns) {
+                        def pattern = config.artifactPatterns.join(',')
+                        archiveArtifacts artifacts: pattern, allowEmptyArchive: true
                     }
                 }
             }
